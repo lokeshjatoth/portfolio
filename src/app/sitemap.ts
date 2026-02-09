8799af7de43851c084f7d91005c03863cbc129c8
lokeshjatoth <lokeshjatothit@gmail.com>
2026-02-10 01:30:31 +0530

    Some changes in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,4 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://portfolio-jatoth-lokeshs-projects.vercel.app";
@@ -9,6 +9,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
-    }
+    },
   ];
 }
